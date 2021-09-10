6aae285ef5c3afe7aff52876db4bde58b0d3b513
Priscila Cerqueira 21 <pricerqueira32@gmail.com>
2021-09-10 17:20:52 -0300

    Reajustando o código da base 64

--- script.js
+++ script.js
@@ -1,51 +1,64 @@
 var texto1= document.getElementById('texto1')
 var incremento= document.getElementById('incremento')
-var botao= document.getElementById('botao')
-var escolhermetodo= document.getElementById('escolhermetodo')
+var botao= document.getElementById('enviar')
+var escolhermetodo= document.getElementById('escolherMetodo')
 var resultado= document.getElementById('resultado')
-var codificar= document.getElementById('codificar')
-var descodificar = document.getElementById('descodificar')
+var codifica= document.getElementById('codi')
+var descodifica = document.getElementById('descodi')
 var resultadofinal= ''
 
-codificar.addEventListener('click',function (){
+codifica.addEventListener('click',function (){
 botao.textContent= 'Codificar mensagem!'
 
 
 })
 
-descodificar.addEventListener('click', function (){
+descodifica.addEventListener('click', function (){
     botao.texContent= 'Descodificar mensagem!'
 })
 
 
 
-document.getElementById('botao').onclick= function(){
+document.getElementById('enviar').onclick= function(event){
+event.preventDefault()
+    var selecionado= escolhermetodo.value
 
+if(selecionado == '1'){
+    if(codifica.checked){
+        resultadofinal = codificar()
+        resultado.textContent= resultadofinal
+    } else if(descodifica.checked) { resultadofinal = decodificar()
+        resultado.textContent= resultadofinal
 
-
-
-
-
-
+    }
 }
 
+else if (selecionado == '2'){
+    if(codifica.checked){
+        resultadofinal = base64()
+        resultado.textContent= resultadofinal
+    } else if(descodifica.checked){ resultadofinal = decodificar64()
+        resultado.textContent= resultadofinal
 
+    }
+}
+}
 
 
 
 //Coletar dados
 //transformar letras em números
 // pesquisar o que é uma split
-var dados // pega os dados do usuário
-var incremento // recebe número que ajuda a codificar 
+ // pega os dados do usuário
+ // recebe número que ajuda a codificar 
     function codificar (){ // vai codificar os dados que usuário 
-        var arr= dados.split("") // var se referindo a 
+        var arr= texto1.value.split("") // var se referindo a 
         // entrando na array com os dados que o usuário forneceu, o split divide uma array
         var arr2=  [] // essa array está vazia porque ainda está esperando para ser usada
         var arr3= []
         for (let i = 0; i < arr.length; i++) {// estrutura do for e só meche no meio
             if (arr[i] !=" "){
-                arr2.push((arr[i].charCodeAt())+incremento)
+                arr2.push(arr[i].charCodeAt() + parseInt(incremento.value))
             }
             else {
                 arr2.push(arr[i].charCodeAt())
@@ -58,14 +71,15 @@ var incremento // recebe número que ajuda a codificar
         return arr3.join("")// se usa uma string vazia () para não separar por virgula o espaço
     }
 
-function descodificar (){
-    var arr= dados.split("") // var se referindo a array
+function decodificar (){
+    var arr= texto1.value.split("") // var se referindo a array
         // entrando na array com os dados que o usuário forneceu, o split divide uma array
-        var arr2=  [] // essa array está vazia porque ainda está esperando para ser usada
+        var arr2= [] // essa array está vazia porque ainda está esperando para ser usada
         var arr3= []
         for (let i = 0; i < arr.length; i++) {// estrutura do for e só meche no meio
             if (arr[i] !=" "){
-                arr2.push((arr[i].charCodeAt())-incremento)
+                arr2.push((arr[i].charCodeAt())- parseInt(incremento.value))
+                console.log(arr2)
             }
             else {
                 arr2.push(arr[i].charCodeAt())
@@ -74,6 +88,7 @@ function descodificar (){
         for (let j = 0; j < arr2.length; j++) {
         
            arr3.push(String.fromCharCode(arr2[j]))
+           console.log(arr3)
         }
         return arr3.join("")// se usa uma string vazia () para não separar por virgula o espaço
 }
@@ -86,11 +101,11 @@ function descodificar (){
 
 
 function base64 () {
-    var code = btoa (dados)
+    var code = btoa (texto1.value)
     return code
 }
 
 function decodificar64() {
-    var decode = atob (dados)
+    var decode = atob (texto1.value)
     return decode
 }
